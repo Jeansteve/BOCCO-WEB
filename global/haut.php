151bcdf2988181416cc1bdb8d7fb2902e1dc499f
jeansteve <ekenjean@gmail.com>
2015-03-01 15:31:09 +0100

    Ajout de global haut.php

---
+++ global/haut.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>BOCCO-COMMANDE</title>
+    <link rel="stylesheet" href="./css/style.css"/>
+
+    <script type="text/javascript" src="./js/jquery-2.1.0.js"></script>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div id="container">
+    <header class="page-header">
+        <h1>COMMANDER VOTRE BOCCO !!!</h1>
+    </header>
+    <section class="row col-lg-10">
